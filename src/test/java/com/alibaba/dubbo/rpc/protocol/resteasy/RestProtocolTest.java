80fd72910cab7153fb90d05adaad2187cc57be6d
sdcuike <sdcuike@aol.com>
2016-04-18 10:20:53 +0800

    test_host_for_LISTEN_ADDRESS

--- src/test/java/com/alibaba/dubbo/rpc/protocol/resteasy/RestProtocolTest.java
+++ src/test/java/com/alibaba/dubbo/rpc/protocol/resteasy/RestProtocolTest.java
@@ -1,7 +1,15 @@
 package com.alibaba.dubbo.rpc.protocol.resteasy;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+import org.jboss.resteasy.core.ResourceMethodRegistry;
+import org.jboss.resteasy.plugins.server.embedded.SecurityDomain;
+import org.jboss.resteasy.plugins.server.netty.NettyJaxrsServer;
+import org.jboss.resteasy.spi.ResteasyDeployment;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -22,7 +30,7 @@ import com.alibaba.dubbo.rpc.ServiceClassHolder;
 public class RestProtocolTest {
 
     @Test
-    public void testRestProtocol() {
+    public void testRestProtocol() throws InterruptedException {
         RestService server = new RestServiceImpl();
         ProxyFactory proxyFactory = ExtensionLoader.getExtensionLoader(ProxyFactory.class).getAdaptiveExtension();
         Protocol protocol = ExtensionLoader.getExtensionLoader(Protocol.class).getAdaptiveExtension();
@@ -36,10 +44,55 @@ public class RestProtocolTest {
         requestDto.setName("doctor who");
         ModelResult<String> modelResult = client.test(requestDto);
 
-        Assert.assertEquals("test" + ToStringBuilder.reflectionToString(requestDto, ToStringStyle.SHORT_PREFIX_STYLE), modelResult.getData());
+        String expected = "test " + ToStringBuilder.reflectionToString(requestDto, ToStringStyle.SHORT_PREFIX_STYLE);
+        System.out.println(expected);
+        Assert.assertEquals(expected, modelResult.getData());
         invoker.destroy();
 
+        TimeUnit.MINUTES.sleep(1);
         exporter.unexport();
+    }
+
+    /**
+     * // lsof -i:8990
+     * // COMMAND PID USER FD TYPE DEVICE SIZE/OFF NODE NAME
+     * 
+     * @throws InterruptedException
+     */
+    @Test
+    public void test_host_for_LISTEN_ADDRESS() throws InterruptedException {
+
+        // String hostname = "10.100.0.25";
+        // String hostname = "127.0.0.1";//java 29782 username 146u IPv6 0x41980098a5e8db51 0t0 TCP localhost:8990 (LISTEN)
+        // String hostname = "localhost";
+
+        String hostname = "0.0.0.0";// java 29827 username 146u IPv6 0x41980098a5e8db51 0t0 TCP *:8990 (LISTEN)
+        int port = 8990;
+        SecurityDomain securityDomain = null;
+        String rootResourcePath = "/";
+        int ioWorkerCount = Runtime.getRuntime().availableProcessors() * 2;
+        int executorThreadCount = 12;
+
+        NettyJaxrsServer nettyJaxrsServer = new NettyJaxrsServer();
+
+        nettyJaxrsServer.setHostname(hostname);
+        nettyJaxrsServer.setPort(port);
+        nettyJaxrsServer.setSecurityDomain(securityDomain);
+        nettyJaxrsServer.setRootResourcePath(rootResourcePath);
+
+        nettyJaxrsServer.setIoWorkerCount(ioWorkerCount);
+        nettyJaxrsServer.setExecutorThreadCount(executorThreadCount);
+
+        ResteasyDeployment deployment = nettyJaxrsServer.getDeployment();
+        List<Object> resources = new ArrayList<Object>();
+        deployment.setResources(resources);
+        nettyJaxrsServer.start();
+        ResourceMethodRegistry resourceMethodRegistry = (ResourceMethodRegistry) deployment.getRegistry();
+        resourceMethodRegistry.setWiderMatching(true);// 不设置也可以
+
+        deployment.getRegistry().addSingletonResource(new RestServiceImpl());
+        TimeUnit.MINUTES.sleep(1);
+
     }
 
 }
